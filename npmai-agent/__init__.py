ecc3fe13edda341eeb88ddb8b5058e8cedfee853
Sonu Kumar <sonuramashishnpm@gmail.com>
2026-06-07 16:55:47 +0530

    Update __init__.py

--- npmai-agent/__init__.py
+++ npmai-agent/__init__.py
@@ -1,1 +1,2 @@
+__version__ = "0.0.1"
 from .npmai-agent import EmailTool, FileTool, WebTool, SpreadsheetTool, GitHubTool, SlackTool, PDFTool, ImageTool, SystemTool, TelegramTool, QRTool, RAGTool, SSHTool, DiscordTool, WhatsAppTool, NotionTool, TwitterTool, SchedulerTool, JiraTool, VoiceTool, WatcherTool, CredStore, Workspace
